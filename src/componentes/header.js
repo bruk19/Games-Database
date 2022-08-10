4251ef2d54307d5f08bdde96a8b2fa6ae8e431a7
bruk19 <brukteshome21@gmail.com>
2022-08-10 22:36:03 +0300

    Add header files

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function header() {
   return (
-    <div>header</div>
+    <div>header
+        `https://api.rawg.io/api/games?key=f701489903124362ba098939ed7babd7`
+    </div>
   )
 }
 
